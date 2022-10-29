b3a4b4407f74b4e4f797ea01edf266f100f8f2ab
Tran Quang <songoku2192@gmail.com>
2022-10-29 18:12:10 +0700

    feat: init router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CartComponent } from './screen/cart/cart.component';
+import { OrderComponent } from './screen/components/order/order.component';
 
 const routes: Routes = [
   {
-    path: 'cart',
-    component: CartComponent,
+    path: 'order/:id',
+    component: OrderComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,40 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSliderModule } from '@angular/material/slider';
-import { CartComponent } from './screen/cart/cart.component';
-import { TransformStatusCartPipe } from './pipe/transform-status-cart.pipe';
+import { OrderComponent } from './screen/components/order/order.component';
+import { TransformDatePipe } from './pipe/transform-date.pipe';
+
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+import { TransformPaymentPipe } from './pipe/transform-payment.pipe';
+import { DialogComponent } from './screen/components/dialog/dialog.component';
+import { BoxChatComponent } from './screen/components/box-chat/box-chat.component';
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
-  declarations: [AppComponent, CartComponent, TransformStatusCartPipe],
+  declarations: [
+    AppComponent,
+    OrderComponent,
+    TransformDatePipe,
+    TransformPaymentPipe,
+    DialogComponent,
+    BoxChatComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
     MatDialogModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
