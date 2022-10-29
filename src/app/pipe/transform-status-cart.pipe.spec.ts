3f8d4a4c6043bdca1cf0a18962af6e2f7a921cdd
Tran Quang <taquang.hskx.2000@gmail.com>
2022-10-29 18:22:53 +0700

    refactor: refactor component
    das

--- src/app/pipe/transform-status-cart.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TransformStatusCartPipe } from './transform-status-cart.pipe';
-
-describe('TransformStatusCartPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TransformStatusCartPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
